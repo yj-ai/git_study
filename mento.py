231018f82137fde45ada44897216ec10f0f204c6
yj_ai <nayoungjun11@naver.com>
2023-11-15 17:54:12 +0900

    2번째

--- mento.py
+++ mento.py
@@ -1,13 +1,12 @@
 
 def str_cal(s):
-    arr = []
+    arr1 = []
+    arr2 = []
     
     for a in s:
         if a=='*' or a=='/':
-            arr.append(a)
-            s.remove(a)
+            arr1.append(a)
+        else:
+            arr2.append(a)
     
-    for b in s:
-        arr.append(b)
-    
-    return arr
+    return arr1+arr2
