608bc6b81f5bbaade6471e09ade88890cce4111a
hooniverse <sung010326@gmail.com>
2023-11-22 17:59:06 +0900

    성훈 함수 구현
    인원수를 받아 국어,수학,과학,영어 과목에 대해 점수를 랜덤으로 저장한 리스트 4개를 생성하고 numpy 배열로 리턴

---
+++ sh2.py
@@ -0,0 +1,26 @@
+def sh2(count):
+    import numpy as np
+    import random
+
+    kor = []
+    math = []
+    science = []
+    english = []
+
+    for i in range(1, count + 1):
+        a = int(random.randint(1, 101))
+        b = int(random.randint(1, 101))
+        c = int(random.randint(1, 101))
+        d = int(random.randint(1, 101))
+        kor.append(a)
+        math.append(b)
+        science.append(c)
+        english.append(d)
+    kor = np.array(kor)
+    math = np.array(math)
+    science = np.array(science)
+    english = np.array(english)
+    return kor, math, science, english
+
+
+print(sh2(1000))
